95031cf81f6149e008afe84ec79ea11d653ad9c6
jubisclaintom <gabrielcrivespaschoal@gmail.com>
2025-02-27 16:55:50 -0300

    Add files via upload

---
+++ projeto_integrador_carrinho/js/carregarprodutos.js
@@ -0,0 +1,239 @@
+let produtos = [
+    { 
+        "nome": "Ambroxol Pediátrico", 
+        "preco": 11.50, 
+        "URL": "imagens/Produtos/Remedios/remedio_ambroxol.jpg", 
+        "descrição": "Remédio Antibiótico"
+    },
+    { 
+        "nome": "Bancoprozol", 
+        "preco": 16.00, 
+        "URL": "imagens/Produtos/Remedios/bancoprozol.jpg", 
+        "descrição": "Remédio Antibiótico" 
+    },
+    { 
+        "nome": "Ciprofloxacino", 
+        "preco": 18.89, 
+        "URL": "imagens/Produtos/Remedios/ciprofloxacino.jpg", 
+        "descrição": "Remédio Antibiótico" 
+    },
+    { 
+        "nome": "Dipirona", 
+        "preco": 5.25, 
+        "URL": "imagens/Produtos/Remedios/dipirona.jpg", 
+        "descrição": "Remédio Antibiótico" 
+    },
+    { 
+        "nome": "Ibuprofeno", 
+        "preco": 7.59, 
+        "URL": "imagens/Produtos/Remedios/ibuprofeno.jpeg", 
+        "descrição": "Remédio Antibiótico" 
+    },
+    { 
+        "nome": "Propanolol", 
+        "preco": 15.50, 
+        "URL": "imagens/Produtos/Remedios/propanolol.png", 
+        "descrição": "Remédio Antibiótico" 
+    },
+    { 
+        "nome": "Timopronolol", 
+        "preco": 14.29, 
+        "URL": "imagens/Produtos/Remedios/timopronolol.jpg", 
+        "descrição": "Remédio Antibiótico" 
+    },
+    { 
+        "nome": "Vonal", 
+        "preco": 73.90, 
+        "URL": "imagens/Produtos/Remedios/vonal.jpeg", 
+        "descrição": "Remédio Antibiótico" 
+    },
+    {
+        "nome": "Tadala", 
+        "preco" : 30.98,
+        "URL": "imagens/Produtos/Remedios/tadala.jpeg"
+    },
+
+
+
+    { 
+        "nome": "Esmalte Impala Rosa", 
+        "preco": 4.50, 
+        "URL": "imagens/Produtos/Cosmeticos/esmalte impala rosa.jpg", 
+        "descrição": "Esmalte"
+    },
+    { 
+        "nome": "Esmalte Risque Nude", 
+        "preco": 5.20, 
+        "URL": "imagens/Produtos/Cosmeticos/esmalte risque nude.jpg", 
+        "descrição": "Esmalte"
+    },
+    { 
+        "nome": "Esmalte Risque Verde-Água", 
+        "preco": 5.30, 
+        "URL": "imagens/Produtos/Cosmeticos/esmalte risque verde-agua.jpg", 
+        "descrição": "Esmalte"
+    },
+    { 
+        "nome": "Shampoo Seda", 
+        "preco": 8.90, 
+        "URL": "imagens/Produtos/Cosmeticos/shampo seda.jpg", 
+        "descrição": "Shampoo"
+    },
+    { 
+        "nome": "Shampoo 3 em 1", 
+        "preco": 10.50, 
+        "URL": "imagens/Produtos/Cosmeticos/shampoo 3 em 1.jpg", 
+        "descrição": "Shampoo"
+    },
+    { 
+        "nome": "Shampoo CR7", 
+        "preco": 12.99, 
+        "URL": "imagens/Produtos/Cosmeticos/shampoo cr7.jpg", 
+        "descrição": "Shampoo"
+    },
+    { 
+        "nome": "Shampoo Mentos", 
+        "preco": 9.75, 
+        "URL": "imagens/Produtos/Cosmeticos/shampoo mentos.jpg", 
+        "descrição": "Shampoo"
+    },
+    { 
+        "nome": "Shampoo Pantene", 
+        "preco": 14.30, 
+        "URL": "imagens/Produtos/Cosmeticos/shampoo pantene.jpg", 
+        "descrição": "Shampoo"
+    },
+    { 
+        "nome": "Tratamento Babosa", 
+        "preco": 15.99, 
+        "URL": "imagens/Produtos/Cosmeticos/tratamento babosa.jpg", 
+        "descrição": "Tratamento Capilar"
+    },
+
+    
+
+    { 
+        "nome": "Coca Cola", 
+        "preco": 6.99, 
+        "URL": "imagens/Produtos/Alimentos/coca cola.jpg", 
+        "descrição": "Refrigerante"
+    },
+    { 
+        "nome": "Fanta", 
+        "preco": 5.89, 
+        "URL": "imagens/Produtos/Alimentos/Fanta.png", 
+        "descrição": "Refrigerante"
+    },
+    { 
+        "nome": "Fini Dentadura", 
+        "preco": 3.50, 
+        "URL": "imagens/Produtos/Alimentos/Fini dentadura.jpg", 
+        "descrição": "Doce"
+    },
+    { 
+        "nome": "Fini Sapo", 
+        "preco": 3.90, 
+        "URL": "imagens/Produtos/Alimentos/fini sapo.jpg", 
+        "descrição": "Doce"
+    },
+    { 
+        "nome": "Guarana Jesus", 
+        "preco": 7.50, 
+        "URL": "imagens/Produtos/Alimentos/guarana Jesus.jpg", 
+        "descrição": "Refrigerante"
+    },
+    { 
+        "nome": "Halls Preta", 
+        "preco": 2.99, 
+        "URL": "imagens/Produtos/Alimentos/halls preta.jpg", 
+        "descrição": "Balas"
+    },
+    { 
+        "nome": "Haribo", 
+        "preco": 6.20, 
+        "URL": "imagens/Produtos/Alimentos/haribo.jpg", 
+        "descrição": "Gomas de Mascar"
+    },
+    { 
+        "nome": "Fanta Laranja Lata", 
+        "preco": 4.99, 
+        "URL": "imagens/Produtos/Alimentos/refrigerante-fanta-laranja-lata-310ml-7894900031157_5.jpg", 
+        "descrição": "Refrigerante"
+    },
+    { 
+        "nome": "Trident", 
+        "preco": 2.30, 
+        "URL": "imagens/Produtos/Alimentos/trident.jpg", 
+        "descrição": "Goma de Mascar"
+    },
+
+    { 
+        "nome": "Acetona", 
+        "preco": 3.99, 
+        "URL": "imagens/Produtos/Higiene/acetona.png", 
+        "descrição": "Higiene"
+    },
+    { 
+        "nome": "Álcool em Gel", 
+        "preco": 6.50, 
+        "URL": "imagens/Produtos/Higiene/alcool em gel.jpg", 
+        "descrição": "Higiene"
+    },
+    { 
+        "nome": "Dove Feminino", 
+        "preco": 9.90, 
+        "URL": "imagens/Produtos/Higiene/dove feminino.jpg", 
+        "descrição": "Higiene"
+    },
+    { 
+        "nome": "Lencinho", 
+        "preco": 4.30, 
+        "URL": "imagens/Produtos/Higiene/lencinho .jpg", 
+        "descrição": "Higiene"
+    },
+    { 
+        "nome": "Lencinho Azul", 
+        "preco": 4.50, 
+        "URL": "imagens/Produtos/Higiene/lencinho azul.jpg", 
+        "descrição": "Higiene"
+    },
+    { 
+        "nome": "Lencinho", 
+        "preco": 4.30, 
+        "URL": "imagens/Produtos/Higiene/lencinho.jpg", 
+        "descrição": "Higiene"
+    },
+    { 
+
+        "nome": "Rexona", 
+        "preco": 8.99, 
+        "URL": "imagens/Produtos/Higiene/rexona.jpg", 
+        "descrição": "Higiene"
+    },
+    { 
+        "nome": "Roll-On Feminino", 
+        "preco": 7.50, 
+        "URL": "imagens/Produtos/Higiene/rollo-on feminino.jpg", 
+        "descrição": "Higiene"
+    },
+    { 
+        "nome": "Roll-On Masculino", 
+        "preco": 7.50, 
+        "URL": "imagens/Produtos/Higiene/rollo-on masculino.jpg", 
+        "descrição": "Higiene"
+    },
+]
+
+// todos os campos dos cards
+for(i = 0; i < 36; i++){
+    produto = document.getElementById("produto" + i)
+    produto.innerHTML = `
+    <img src="${produtos[i].URL}" class="card-img-top" alt="${produtos[i].descrição}">
+    <div class='card-body'>
+        <p class='card-text'>${produtos[i].nome} R$${produtos[i].preco.toFixed(2)}</p>
+        <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#exampleModal' onclick='carrinho(${i})'>
+            Comprar
+        </button>
+    </div>
+    `;
+}
